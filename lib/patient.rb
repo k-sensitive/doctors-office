abd7dd310b0fe6d4b8912721cc27114c30393c59
Andrew Lee & Kim Dang <student@epicodus.com>
2015-05-05 12:12:44 -0700

    save and display all patients.

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,11 +12,20 @@ class Patient
     patient_lists = []
     all_patients.each() do |patient|
       name = patient.fetch('name')
+      birthday = patient.fetch('birthday')
       id = patient.fetch('id').to_i()
-      patient_lists.push(Patient.new({:name => name, :birthday => birthday}))
+      doctor_id = patient.fetch('doctor_id').to_i()
+      patient_lists.push(Patient.new({:name => name, :birthday => birthday, :doctor_id => doctor_id}))
     end
     patient_lists
   end
 
+  define_method(:save) do
+    DB.exec("INSERT INTO patients (name, birthday, doctor_id) VALUES ('#{@name}', '#{birthday}',null)")
+  end
+
+  define_method(:==) do |another_patient|
+    self.name().==(another_patient.name()).&(self.birthday().==(another_patient.birthday()))
+  end
 
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -10,14 +10,22 @@ describe('Patient') do
 
   describe('#birthday') do
     it('will show the birthday of the patient') do
-      newpatient = Patient.new({:birthday => 2015-05-05})
-      expect(newpatient.birthday()).to(eq(2015-05-05))
+      newpatient = Patient.new({:birthday => '2015-05-05'})
+      expect(newpatient.birthday()).to(eq('2015-05-05'))
     end
   end
 
   describe('.all') do
     it('will return an empty array where a list of patients will be') do
       expect(Patient.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('will save the patient info into the array') do
+      newpatient = Patient.new({:name => "John", :birthday => '2015-05-05 00:00:00'})
+      newpatient.save()
+      expect(Patient.all()).to(eq([newpatient]))
     end
   end
 end
