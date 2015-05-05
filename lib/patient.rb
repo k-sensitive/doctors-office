1c81b1a1590ed9dcc3acf5e94ece3c53ffb861c5
Andrew Lee & Kim Dang <student@epicodus.com>
2015-05-05 11:46:38 -0700

    add the birthday to the patient

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,9 @@
 class Patient
-  attr_reader(:name)
+  attr_reader(:name,:birthday)
 
   define_method(:initialize) do |attributes|
     @name = attributes[:name]
+    @birthday = attributes[:birthday]
   end
 
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -7,4 +7,11 @@ describe('Patient') do
       expect(newpatient.name()).to(eq("John"))
     end
   end
+
+  describe('#birthday') do
+    it('will show the birthday of the patient') do
+      newpatient = Patient.new({:birthday => 2015-05-05})
+      expect(newpatient.birthday()).to(eq(2015-05-05))
+    end
+  end
 end
